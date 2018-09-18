d45605911d154c110923998997224f62a76caf25
dmitry-karabanovich <karabanovich.dmitry@gmail.com>
2018-09-18 12:25:07 +0300

    Create ServiceFactory and BaseController

---
+++ HiQo.StaffManagement.WebApi/App_Start/Startup.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(HiQo.StaffManagement.WebApi.App_Start.Startup))]
+
+namespace HiQo.StaffManagement.WebApi.App_Start
+
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+        }
+    }
+}
